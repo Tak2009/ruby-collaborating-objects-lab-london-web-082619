d34028c7cc9a279b17076166c823f140c09b2ca4
github username <github email address>
2019-08-31 19:27:54 +0100

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,19 @@
+class MP3Importer
+
+    attr_reader :path
+
+    def initialize(path)
+        @path = path
+    end
+
+    def files
+        files_array = Dir[path+"/*"]
+        files_array.map {|filename| filename.sub(self.path+"/", '')}
+    end
+
+
+    def import
+        self.files.each {|filename| Song.new_by_filename(filename)}
+    end
+
+end
